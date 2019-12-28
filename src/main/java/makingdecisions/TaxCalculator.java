0bee66e5ec3be1ab07babbc4b8d64232b88993c6
savamadalinmihai <savamadalinmihai@gmail.com>
2019-12-28 23:30:24 +0200

    EX 14.

--- src/main/java/makingdecisions/TaxCalculator.java
+++ src/main/java/makingdecisions/TaxCalculator.java
@@ -7,19 +7,18 @@ public class TaxCalculator {
     double amount = getAmount();
     String state = getState();
     double simpleTotal = Math.round(amount * 100.0) / 100.0;
-    double wisconsinTax = Math.round((5.5/100) * simpleTotal * 100.0) / 100.0;
+    double wisconsinTax = Math.round((5.5 / 100) * simpleTotal * 100.0) / 100.0;
     double total = Math.round((simpleTotal + wisconsinTax) * 100.0) / 100.0;
 
 
-    public void calculateTax(){
+    public void calculateTax() {
         if (state.equals("WI")) {
             System.out.println("The subtotal is " + dollar + simpleTotal);
             System.out.println("The tax is " + dollar + wisconsinTax);
             System.out.println("The total is " + dollar + total);
-        }else{
-            System.out.println("The total is " + dollar + simpleTotal);
+            return;
         }
-
+        System.out.println("The total is " + dollar + simpleTotal);
     }
 
     public double getAmount() {
