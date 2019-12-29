209cc505554b2d83bc399654fa8928a7bdc221ed
savamadalinmihai <savamadalinmihai@gmail.com>
2019-12-29 23:12:21 +0200

    EX 15.

---
+++ src/main/java/makingdecisions/PasswordValidation.java
@@ -0,0 +1,36 @@
+package makingdecisions;
+import java.util.Scanner;
+
+public class PasswordValidation {
+    String user = getUser();
+
+    public void validate(){
+        String password = getPassword();
+        if (!password.equals("abc$123")){
+            System.out.println("I don't know you!");
+            System.out.println("Please try again!");
+            System.out.println("");
+            validate();
+        }else{
+            System.out.println("Welcome, " + user + "!");
+        }
+    }
+
+    public String getUser(){
+        System.out.println("Hi! Please enter the username!");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next();
+    }
+    public String getPassword(){
+        System.out.println("Please enter the password.");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next();
+    }
+
+
+    public static void main(String[] args) {
+        PasswordValidation passwordValidation = new PasswordValidation();
+        passwordValidation.validate();
+
+    }
+}
