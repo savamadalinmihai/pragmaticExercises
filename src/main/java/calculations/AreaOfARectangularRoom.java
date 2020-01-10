7226205b22e0e74ca876ad3d58ca1188a47f6615
Sava Madalin <savamadalinmihai@gmail.com>
2020-01-10 15:45:54 +0200

    BMI Calculator + update

--- src/main/java/calculations/AreaOfARectangularRoom.java
+++ src/main/java/calculations/AreaOfARectangularRoom.java
@@ -4,25 +4,27 @@ import java.util.Scanner;
 
 public class AreaOfARectangularRoom {
     int lengthInFeet = getLengthInFeet();
-    int widthInFeet = getWidthInFeet();;
+    int widthInFeet = getWidthInFeet();
+    ;
     int area = lengthInFeet * widthInFeet;
     double areaInMeters = area * 0.09290304;
     double roundedAreaInMeters = Math.round(areaInMeters * 1000.0) / 1000.0;
 
-public void calculateArea(){
-    System.out.println("You entered dimensions of " + lengthInFeet + " feet by " + widthInFeet + " feet.");
-    System.out.println("The area in feet is " + area);
-    System.out.println("The area in meters is " + roundedAreaInMeters);
+    public void calculateArea() {
+        System.out.println("You entered dimensions of " + lengthInFeet + " feet by " + widthInFeet + " feet.");
+        System.out.println("The area in feet is " + area);
+        System.out.println("The area in meters is " + roundedAreaInMeters);
 
-}
+    }
 
 
-    private int getLengthInFeet(){
+    private int getLengthInFeet() {
         System.out.println("Please enter the length in feet: ");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
-    private int getWidthInFeet(){
+
+    private int getWidthInFeet() {
         System.out.println("Please enter the width in feet: ");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
--- src/main/java/makingdecisions/BMICalculator.java
+++ src/main/java/makingdecisions/BMICalculator.java
@@ -4,27 +4,82 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class BMICalculator {
-    double height = getHeight();
-    double weight = getWeight();
-    double bmi = (weight / (height * height)) * 703;
+    String metricOrImperial = getMetricOrImperial();
 
+    public void decideMethod() {
+        if (!metricOrImperial.matches("MmIi")) {
+            System.out.println("Please enter one of the two options!");
+            decideMethod();
+        } else
 
-    public void calculate() {
-        if (bmi < 25 && bmi > 18.5) {
-            System.out.println("Your BMI is " + bmi + ".");
+        if (metricOrImperial.equalsIgnoreCase("M")) {
+            System.out.println("You chose the Metric system. For this, please use cm and kg");
+            calculateMetric();
+        } else if (metricOrImperial.equalsIgnoreCase("I")) {
+            System.out.println("You chose the Imperial system. For this, please specify your height in inches" +
+                    " and your weight in pounds");
+            calculateImperial();
+        }
+    }
+
+    public void calculateMetric() {
+        double height = getHeight();
+        double weight = getWeight();
+        double bmiMetric = (weight / (height * height)) * 10000;
+
+        if (bmiMetric < 25 && bmiMetric > 18.5) {
+            System.out.format("Your BMI is %.1f", bmiMetric);
+            System.out.print(".");
+            System.out.println("");
             System.out.println("You are within the ideal weight range.");
-        } else if (bmi < 18.5) {
-            System.out.println("Your BMI is " + bmi + ".");
+        } else if (bmiMetric < 18.5) {
+            System.out.format("Your BMI is %.1f", bmiMetric);
+            System.out.print(".");
+            System.out.println("");
             System.out.println("You are underweight. You should see a doctor.");
-        } else if (bmi > 25) {
-            System.out.println("Your BMI is " + bmi + ".");
+        } else if (bmiMetric > 25) {
+            System.out.format("Your BMI is %.1f", bmiMetric);
+            System.out.print(".");
+            System.out.println("");
             System.out.println("You are overweight. You should see a doctor.");
         }
     }
 
+    public void calculateImperial() {
+        double height = getHeight();
+        double weight = getWeight();
+        double bmiImperial = Math.round((weight / (height * height)) * 703);
+
+        if (bmiImperial < 25 && bmiImperial > 18.5) {
+            System.out.format("Your BMI is %.1f", bmiImperial);
+            System.out.print(".");
+            System.out.println("");
+            System.out.println("You are within the ideal weight range.");
+        } else if (bmiImperial < 18.5) {
+            System.out.format("Your BMI is %.1f", bmiImperial);
+            System.out.print(".");
+            System.out.println("");
+            System.out.println("You are underweight. You should see a doctor.");
+        } else if (bmiImperial > 25) {
+            System.out.format("Your BMI is %.1f", bmiImperial);
+            System.out.print(".");
+            System.out.println("");
+            System.out.println("You are overweight. You should see a doctor.");
+        }
+    }
+
+    public String getMetricOrImperial() {
+        System.out.println("What units would you like to use?");
+        System.out.println("");
+        System.out.println("For METRIC, please press M. ");
+        System.out.println("For IMPERIAL, please press I. ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next();
+    }
+
     public double getHeight() {
         try {
-            System.out.println("What is your height in inches?");
+            System.out.println("What is your height?");
             Scanner scanner = new Scanner(System.in);
             return scanner.nextDouble();
         } catch (InputMismatchException e) {
@@ -35,7 +90,7 @@ public class BMICalculator {
 
     public double getWeight() {
         try {
-            System.out.println("What is your weight in pounds?");
+            System.out.println("What is your weight?");
             Scanner scanner = new Scanner(System.in);
             return scanner.nextDouble();
         } catch (InputMismatchException e) {
@@ -46,7 +101,7 @@ public class BMICalculator {
 
     public static void main(String[] args) {
         BMICalculator bmiCalculator = new BMICalculator();
-        bmiCalculator.calculate();
+        bmiCalculator.decideMethod();
 
     }
 }
