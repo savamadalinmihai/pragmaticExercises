ba163e83f1e027f6b905010efb7e57c33c0a8da9
savamadalinmihai <savamadalinmihai@gmail.com>
2020-01-13 21:45:49 +0200

    Fixed NumbersToNames

--- src/main/java/makingdecisions/NumbersToNames.java
+++ src/main/java/makingdecisions/NumbersToNames.java
@@ -4,61 +4,8 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class NumbersToNames {
-    int number = getNumber();
-
-    public void validator() {
-        while (number < 1 || number > 12) {
-            System.out.println("Please enter a number between 1 and 12.");
-            translateAgain();
-            if (number >= 1 && number <= 12) {
-                translate();
-                break;
-            }
-        }
-    }
 
     public void translate() {
-        switch (number) {
-            case 1:
-                System.out.println("The name of the month is January.");
-                break;
-            case 2:
-                System.out.println("The name of the month is February.");
-                break;
-            case 3:
-                System.out.println("The name of the month is March.");
-                break;
-            case 4:
-                System.out.println("The name of the month is April.");
-                break;
-            case 5:
-                System.out.println("The name of the month is May.");
-                break;
-            case 6:
-                System.out.println("The name of the month is June.");
-                break;
-            case 7:
-                System.out.println("The name of the month is July.");
-                break;
-            case 8:
-                System.out.println("The name of the month is August.");
-                break;
-            case 9:
-                System.out.println("The name of the month is September.");
-                break;
-            case 10:
-                System.out.println("The name of the month is October.");
-                break;
-            case 11:
-                System.out.println("The name of the month is November.");
-                break;
-            case 12:
-                System.out.println("The name of the month is December.");
-                break;
-        }
-    }
-
-    public void translateAgain() {
         int number = getNumber();
         switch (number) {
             case 1:
@@ -97,6 +44,9 @@ public class NumbersToNames {
             case 12:
                 System.out.println("The name of the month is December.");
                 break;
+            default:
+                System.out.println("There are only 12 months in a year. You need to enter a number from 1 to 12");
+                translate();
         }
     }
 
@@ -114,6 +64,6 @@ public class NumbersToNames {
 
     public static void main(String[] args) {
         NumbersToNames numbersToNames = new NumbersToNames();
-        numbersToNames.validator();
+        numbersToNames.translate();
     }
 }
---
+++ src/main/java/makingdecisions/TroubleshootingCarIssues.java
@@ -0,0 +1,13 @@
+package makingdecisions;
+
+public class TroubleshootingCarIssues {
+
+
+
+
+
+    public static void main(String[] args) {
+        TroubleshootingCarIssues troubleshootingCarIssues = new TroubleshootingCarIssues();
+//        troubleshootingCarIssues.start();
+    }
+}
