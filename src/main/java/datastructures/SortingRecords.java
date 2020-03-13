86267f62f1ea91eb63c7c70873f67575fd824105
Sava Madalin <savamadalinmihai@gmail.com>
2020-03-13 15:58:51 +0200

    sorted rowslist using comparator

--- src/main/java/datastructures/SortingRecords.java
+++ src/main/java/datastructures/SortingRecords.java
@@ -20,30 +20,25 @@ public class SortingRecords {
         row1.put("position", "Manager");
         row1.put("separation_date", LocalDate.of(2016, 12, 31));
 
-
         row2.put("first_name", "Tou");
         row2.put("last_name", "Xiong");
         row2.put("position", "Software Engineer");
         row2.put("separation_date", LocalDate.of(2016, 10, 5));
-
 
         row3.put("first_name", "Michaela");
         row3.put("last_name", "Michaelson");
         row3.put("position", "District Manager");
         row3.put("separation_date", LocalDate.of(2015, 12, 19));
 
-
         row4.put("first_name", "Jake");
         row4.put("last_name", "Jacobson");
         row4.put("position", "Programmer");
         row4.put("separation_date", null);
 
-
         row5.put("first_name", "Jaquelin");
         row5.put("last_name", "Jackson");
         row5.put("position", "DBA");
         row5.put("separation_date", null);
-
 
         row6.put("first_name", "Sally");
         row6.put("last_name", "Weber");
@@ -61,7 +56,11 @@ public class SortingRecords {
     public void sortRecords() {
         rowsList.sort(Comparator.comparing(rowsList -> rowsList.get("last_name").toString()));
 
-        System.out.println(rowsList.toString());
+        System.out.println("Name " + "\t| Position" + "\t| Separation Date ");
+        for (int i = 1; i <= rowsList.size(); i++) {
+            System.out.println(rowsList.listIterator(i) + "\t");
+        }
+        System.out.println(rowsList.listIterator().toString() + "\t");
     }
 
     public static void main(String[] args) {
